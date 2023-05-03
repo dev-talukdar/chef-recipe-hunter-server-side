60e58571930f5938b57e8d6e145911f451ddccca
dev-talukdar <khejurbd@gmail.com>
2023-05-04 04:54:52 +0600

    vercel deploying

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000; 
 
 const chef = require ('./data/chef.json')
+const recipe = require ('./data/recipe.json')
 
 app.use(cors());
 
@@ -11,10 +12,19 @@ app.get('/', (req, res) =>{
     res.send('Dish Delight is Running')
 });
 
-app.get('/chef', (req, res) =>{
-    console.log(chef)
+app.get('/chef', (req, res) =>{ 
     res.send(chef)
 });
+
+app.get('/recipe', (req, res) =>{ 
+    res.send(recipe)
+});
+
+app.get('/recipe/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id)
+
+})
 
 app.listen(port, () => {
     console.log(`Dish Delight api is running on port :${port}`)
