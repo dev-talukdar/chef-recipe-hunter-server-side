71f1846ac7a612961db84c1bf49fcd6cf1333c0f
dev-talukdar <khejurbd@gmail.com>
2023-05-04 00:56:45 +0600

    chef picture is now loading

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000; 
+
+const chef = require ('./data/chef.json')
+
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('Dish Delight is Running')
 });
 
+app.get('/chef', (req, res) =>{
+    console.log(chef)
+    res.send(chef)
+});
 
 app.listen(port, () => {
     console.log(`Dish Delight api is running on port :${port}`)
